2566de839292ac3c09129e738fa1b95a3d3cfa24
dw31-AO722 <wide-aze@student.42.fr>
2015-02-27 22:09:23 +0100

    added ncurses functions prototype for later use

--- srcs/main.c
+++ srcs/main.c
@@ -6,9 +6,18 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 20:32:42 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/27 20:32:57 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/27 22:06:57 by wide-aze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** ncurses functions:
+**
+** initsc() : start curses mode
+** printw(char *str) : print a string
+** refresh() : refresh the screen
+** getch() : wait for user input
+*/
 
 #include "game_2048.h"
 
