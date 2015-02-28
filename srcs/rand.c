c15a85911a934e7866b43ceae69da454fa093955
wide-aze <wide-aze@student.42.fr>
2015-02-28 17:07:12 +0100

    update start_game

--- srcs/rand.c
+++ srcs/rand.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 12:18:24 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/28 15:17:22 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 17:04:32 by wide-aze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,50 @@ t_bool			ft_randbool(void)
 	if (rand() % 2 == 0)
 		return (true);
 	return (false);
+}
+
+static int		count_void_cases(t_env *env)
+{
+	int		i;
+	int		j;
+	int		count;
+
+	count = 0;
+	i = -1;
+	while (++i < env->game.size)
+	{
+		j = -1;
+		while (++j < env->game.size)
+		{
+			if (env->game.map[i][j] == 0)
+				count++;
+		}	
+	}	
+	return (count);
+}
+
+void			put_rand(t_env *env)
+{
+	int		nb;
+	int		pos;
+	int		i;
+	int		j;
+
+	nb = count_void_cases(env);
+	pos = ft_rand(0, nb);
+	i = -1;
+	while (++i < env->game.size)
+	{
+		j = -1;
+		while (++j < env->game.size)
+		{
+			if (env->game.map[i][j] == 0)
+				count++;
+			if (count == pos)
+			{
+				env->game.map[i][j] = ft_randbool() ? 4 : 2;
+				return ;
+			}
+		}	
+	}	
 }
