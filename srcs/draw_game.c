18cf55b5de921bc6ea3fa0e7f86d27c96f1c02e7
jaguillo <juloo.dsi@gmail.com>
2015-02-28 19:55:58 +0100

    Start using colors

--- srcs/draw_game.c
+++ srcs/draw_game.c
@@ -6,12 +6,30 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 15:07:23 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/28 18:22:37 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 19:44:04 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_2048.h"
 #include <ncurses.h>
+
+static void		cell_color(int nb)
+{
+	if (nb == 2)
+		attron(COLOR_PAIR(1));
+	else if (nb == 4)
+		attron(COLOR_PAIR(2));
+	else if (nb == 8)
+		attron(COLOR_PAIR(3));
+	else if (nb == 16)
+		attron(COLOR_PAIR(4));
+	else if (nb == 32 || nb == 64)
+		attron(COLOR_PAIR(5));
+	else if (nb == 128 || nb == 256)
+		attron(COLOR_PAIR(6));
+	else
+		attron(COLOR_PAIR(7));
+}
 
 static void		draw_cell(t_rect rect, int nb)
 {
@@ -19,6 +37,7 @@ static void		draw_cell(t_rect rect, int nb)
 	int				len;
 	int				middle;
 
+	cell_color(nb);
 	middle = rect.height / 2 + rect.y;
 	rect.height += rect.y;
 	while (rect.y < rect.height)
@@ -37,6 +56,7 @@ void			draw_game(t_env *env)
 	t_pt			i;
 
 	clear();
+	attron(COLOR_PAIR(0));
 	WPUT(0, 0, "Score: %d", env->score);
 	cell = RECT(0, 0, env->win_width / env->map_size,
 		env->win_height / env->map_size);
@@ -51,5 +71,6 @@ void			draw_game(t_env *env)
 			draw_cell(cell, MAP_GET(env, i.x, i.y));
 		}
 	}
+	attron(COLOR_PAIR(0));
 	refresh();
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 20:32:42 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/28 19:03:29 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 19:42:31 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,21 @@ static void		init_ncurses(void)
 	initscr();
 	noecho();
 	keypad(stdscr, TRUE);
+	start_color();
+	init_color(1, 933, 894, 854);
+	init_color(2, 933, 878, 784);
+	init_color(3, 949, 694, 474);
+	init_color(4, 960, 584, 388);
+	init_color(5, 500, 500, 500);
+	init_color(6, 500, 500, 500);
+	init_color(7, 500, 500, 500);
+	init_pair(1, COLOR_BLACK, 1);
+	init_pair(2, COLOR_BLACK, 2);
+	init_pair(3, COLOR_WHITE, 3);
+	init_pair(4, COLOR_WHITE, 4);
+	init_pair(5, COLOR_WHITE, 5);
+	init_pair(6, COLOR_WHITE, 6);
+	init_pair(7, COLOR_WHITE, 7);
 }
 
 int				main(void)
